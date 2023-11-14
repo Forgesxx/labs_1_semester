2dfb406239bbfce8b902eebc7e2b8b28c49a062b
ROG\forge <Forgesx236@gmail.com>
2023-11-14 22:10:47 +0200

    im made some changes in my solution and now it work correctly

--- lab_3.5/lab_3.5.js
+++ lab_3.5/lab_3.5.js
@@ -1,8 +1,16 @@
-function BubbleSort(arr) {
+function BubbleSort(arr, des = false) {
     var n = arr.length;
     for (var i = 0; i < n; i++) {
         for (var j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
+            var compareResult;
+
+            if (des) {
+                compareResult = arr[j] < arr[j + 1];
+            } else {
+                compareResult = arr[j] > arr[j + 1];
+            }
+
+            if (compareResult) {
                 var temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
@@ -11,39 +19,53 @@ function BubbleSort(arr) {
     }
 }
 
-function sortText(inputText) {
-    var characters = inputText.split('');
-    var letters = [];
-    var digits = [];
-
-    for (var i = 0; i < characters.length; i++) {
-        var char = characters[i];
-        if (char.match(/[a-zA-Z]/)) {
-            letters.push(char);
-        } else if (char.match(/\d/)) {
-            digits.push(char);
-        }
+function getSymbolKind(str) {
+    let result = 0;
+    let code = str.charCodeAt(0);
+    if ((code > 64 && code < 91) || (code > 96 && code < 123)) {
+        result = 1; // буква
+    } else if ((code > 47 && code < 58)) {
+        result = 2; // цифра
     }
-
-    BubbleSort(letters);
-    BubbleSort(digits);
-
-    var resultText = [];
-
-    for (var i = 0; i < characters.length; i++) {
-        var char = characters[i];
-        if (char.match(/[a-zA-Z]/)) {
-            resultText.push(letters.shift());
-        } else if (char.match(/\d/)) {
-            resultText.push(digits.pop());
-        } else {
-            resultText.push(char);
-        }
-    }
-
-    return resultText.join('');
+    return result;
 }
-//var inputText = prompt("");
-var inputText = "dcba1234";
+
+function sortText(inputText) {
+    let currentGroupKind = 0;
+    let currentGroup = "";
+    let output = "";
+
+    for (let i = 0; i < inputText.length; i++) {
+        let symbol = inputText[i];
+        let symbolKind = getSymbolKind(symbol);
+
+        if (symbolKind !== currentGroupKind) {
+            if (currentGroupKind === 1 || currentGroupKind === 2) {
+                // Используем вашу функцию BubbleSort для сортировки
+                const groupArray = currentGroup.split('');
+                BubbleSort(groupArray, currentGroupKind === 2);
+                currentGroup = groupArray.join('');
+            }
+            output = output + currentGroup;
+            currentGroup = "";
+            currentGroupKind = symbolKind;
+        }
+
+        currentGroup = currentGroup + symbol;
+    }
+
+    if (currentGroupKind === 1 || currentGroupKind === 2) {
+        const groupArray = currentGroup.split('');
+        BubbleSort(groupArray, currentGroupKind === 2);
+        currentGroup = groupArray.join('');
+    }
+
+    output = output + currentGroup;
+
+    return output;
+}
+
+var inputText = "«cba321 /’abfc3785,’’3946f";
 var outputText = sortText(inputText);
-console.log(outputText);
+console.log("InputText:", inputText);
+console.log("OutputText:", outputText);
